0680c343f0265c739e23850a58af0c6120ddeae5
Shekhar Kumar <shekharkumargupta@gmail.com>
2020-05-02 15:42:21 +0530

    first commit

---
+++ src/test/java/com/codeactuator/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeactuator.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
